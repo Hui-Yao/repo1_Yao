16dab26ef8ca854b4526a5b7a55b9b3771a9aaf0
Hui-Yao <im_huiyao@163.com>
2020-06-11 17:12:53 +0800

    tfLearning

--- program/02_python/01_学习总结/Try.py
+++ program/02_python/01_学习总结/Try.py
@@ -1,5 +1,9 @@
-def isvalid(s):
-    bracket = {'(':')', ')':'(', '[':']', ']':'[', '{':'}', '}':'{'}
-    bra_list = '([{'
-    for i in s:
+import tensorflow as tf
+import os
+from tensorflow.python.client import device_lib
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "99"
+
+if __name__ == "__main__":
+    print(device_lib.list_local_devices())
 
